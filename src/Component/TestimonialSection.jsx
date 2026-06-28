a74492cade868bc9505eb5f53bd2322a4b454147
sujalbhardwaj-hub <ptdsujal4103@gmail.com>
2026-06-28 11:12:43 +0530

    Add Testimonial Section

---
+++ src/Component/TestimonialSection.jsx
@@ -0,0 +1,13 @@
+function TestimonialSection() {
+  return (
+    <div>
+      <h2>Testimonials</h2>
+
+      <p>"Excellent learning platform!"</p>
+
+      <p>"React became easy after SkillPath."</p>
+    </div>
+  );
+}
+
+export default TestimonialSection;

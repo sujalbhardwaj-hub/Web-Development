acde0e634f49b1c28bf21f9b09e1a25978995ac7
sujalbhardwaj-hub <ptdsujal4103@gmail.com>
2026-06-28 11:14:52 +0530

    Add About Page

---
+++ src/Pages/Aboutpage.jsx
@@ -0,0 +1,14 @@
+function AboutPage() {
+  return (
+    <div>
+      <h1>About SkillPath</h1>
+
+      <p>
+        SkillPath helps students learn web
+        development and modern technologies.
+      </p>
+    </div>
+  );
+}
+
+export default AboutPage;
